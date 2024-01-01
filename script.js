af283937022f55db866f91ebbccfc08af80db418
alejandro83457 <martinezalejandro2001@hotmail.com>
2023-12-31 21:12:27 -0800

    Add photo credits and finish password mismatch styles

--- script.js
+++ script.js
@@ -3,18 +3,35 @@ let confirmInput = document.querySelector("#confirm");
 let noMatchText = document.querySelector(".no-match");
 
 let passwordText = "";
+let confirmText = "";
 
 passwordInput.addEventListener("input", (e) => {
     passwordText = e.target.value;
-});
-confirmInput.addEventListener("input", (e) => {
-    if (e.target.value != passwordText) {
-        passwordInput.style.border = "1px solid red";
-        confirmInput.style.border = "1px solid red";
+
+    if (confirmText != passwordText) {
+        passwordInput.style.border = "2px solid red";
+        passwordInput.style.borderRadius = "5px";
+        confirmInput.style.border = "2px solid red";
+        confirmInput.style.borderRadius = "5px";
         noMatchText.textContent = "* Passwords do not match";
     } else {
-        passwordInput.style.border = "1px solid green";
-        confirmInput.style.border = "1px solid green";
+        passwordInput.style.border = "2px solid green";
+        confirmInput.style.border = "2px solid green";
+        noMatchText.textContent = "";
+    }
+});
+confirmInput.addEventListener("input", (e) => {
+    confirmText = e.target.value;
+
+    if (confirmText != passwordText) {
+        passwordInput.style.border = "2px solid red";
+        passwordInput.style.borderRadius = "5px";
+        confirmInput.style.border = "2px solid red";
+        confirmInput.style.borderRadius = "5px";
+        noMatchText.textContent = "* Passwords do not match";
+    } else {
+        passwordInput.style.border = "2px solid green";
+        confirmInput.style.border = "2px solid green";
         noMatchText.textContent = "";
     }
 });
