9cb92906d2552e6c86acd88b42fe0352a6f865bc
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-01-01 21:46:29 -0800

    Allow event listeners to share a callback function

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ let noMatchText = document.querySelector(".no-match");
 let passwordText = "";
 let confirmText = "";
 
-passwordInput.addEventListener("input", (e) => {
-    passwordText = e.target.value;
+// Function is used by text event handlers to check
+// if passwords match
+const checkPasswordFields = (e, type) => {
+    if (type == "pass") passwordText = e.target.value;
+    else confirmText = e.target.value;
 
     if (confirmText != passwordText) {
         passwordInput.style.border = "2px solid red";
@@ -19,19 +22,8 @@ passwordInput.addEventListener("input", (e) => {
         confirmInput.style.border = "2px solid green";
         noMatchText.textContent = "";
     }
-});
-confirmInput.addEventListener("input", (e) => {
-    confirmText = e.target.value;
+};
 
-    if (confirmText != passwordText) {
-        passwordInput.style.border = "2px solid red";
-        passwordInput.style.borderRadius = "5px";
-        confirmInput.style.border = "2px solid red";
-        confirmInput.style.borderRadius = "5px";
-        noMatchText.textContent = "* Passwords do not match";
-    } else {
-        passwordInput.style.border = "2px solid green";
-        confirmInput.style.border = "2px solid green";
-        noMatchText.textContent = "";
-    }
-});
+// Event handlers
+passwordInput.addEventListener("input", (e) => checkPasswordFields(e, "pass"));
+confirmInput.addEventListener("input", (e) => checkPasswordFields(e, "conf"));
